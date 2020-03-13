43ff6e526b43ecee4b6fb705680658371d485bb4
Nemanja Radoičić <39140245+NeZero99@users.noreply.github.com>
2020-03-13 01:45:29 +0100

    Dodat intent za edit
    Dodat intent u kome mogu da se dodaju i oduzimaju obroci

---
+++ MenzaObroci/app/src/main/java/proyectn/com/menzaobroci/Edit.java
@@ -0,0 +1,123 @@
+package proyectn.com.menzaobroci;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class Edit extends AppCompatActivity implements View.OnClickListener {
+
+    public static final String MAIN_PREFS = "mainPrefs";
+    public static final String DORUCAK_PREF = "dorucak";
+    public static final String RUCAK_PREF = "rucak";
+    public static final String VECERA_PREF = "vecera";
+
+    private int dorucak;
+    private int rucak;
+    private int vecera;
+
+    private TextView dor;
+    private TextView ruc;
+    private TextView vec;
+
+    private SharedPreferences sharedPreferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+        //toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Izmeni obroke");
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        //textview
+        dor = (TextView) findViewById(R.id.tvDorucakCnt);
+        ruc = (TextView) findViewById(R.id.tvRucakCnt);
+        vec = (TextView) findViewById(R.id.tvVeceraCnt);
+        //sharedPref
+        sharedPreferences = this.getSharedPreferences(MAIN_PREFS, 0);
+        dorucak = sharedPreferences.getInt(DORUCAK_PREF, 0);
+        rucak = sharedPreferences.getInt(RUCAK_PREF, 0);
+        vecera = sharedPreferences.getInt(VECERA_PREF, 0);
+        postaviView();
+        //listeneri za Dugmice;
+        Button m1 = (Button) findViewById(R.id.btnDorucakMinus);
+        Button m2 = (Button) findViewById(R.id.btnRucakMinus);
+        Button m3 = (Button) findViewById(R.id.btnVeceraMinus);
+        Button p1 = (Button) findViewById(R.id.btnDorucakPlus);
+        Button p2 = (Button) findViewById(R.id.btnRucakPlus);
+        Button p3 = (Button) findViewById(R.id.btnVeceraPlus);
+        Button save = (Button) findViewById(R.id.btnSacuvaj);
+        m1.setOnClickListener(this);
+        m2.setOnClickListener(this);
+        m3.setOnClickListener(this);
+        p1.setOnClickListener(this);
+        p2.setOnClickListener(this);
+        p3.setOnClickListener(this);
+        save.setOnClickListener(this);
+    }
+
+    private void postaviView(){
+        dor.setText(Integer.toString(dorucak));
+        ruc.setText(Integer.toString(rucak));
+        vec.setText(Integer.toString(vecera));
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnDorucakMinus:
+                if(dorucak != 0){
+                    dorucak--;
+                    postaviView();
+                }
+                break;
+            case R.id.btnDorucakPlus:
+                dorucak++;
+                postaviView();
+                break;
+            case R.id.btnRucakMinus:
+                if(rucak != 0) {
+                    rucak--;
+                    postaviView();
+                }
+                break;
+            case R.id.btnRucakPlus:
+                rucak++;
+                postaviView();
+                break;
+            case R.id.btnVeceraMinus:
+                if(vecera != 0) {
+                    vecera--;
+                    postaviView();
+                }
+                break;
+            case R.id.btnVeceraPlus:
+                vecera++;
+                postaviView();
+                break;
+            case R.id.btnSacuvaj:
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putInt(DORUCAK_PREF, dorucak);
+                editor.putInt(RUCAK_PREF, rucak);
+                editor.putInt(VECERA_PREF, vecera);
+                editor.apply();
+
+                Toast.makeText(this, "Obroci su sacuvani", Toast.LENGTH_SHORT);
+                break;
+            default:
+                break;
+        }
+    }
+}
--- MenzaObroci/app/src/main/java/proyectn/com/menzaobroci/MainActivity.java
+++ MenzaObroci/app/src/main/java/proyectn/com/menzaobroci/MainActivity.java
@@ -1,10 +1,16 @@
 package proyectn.com.menzaobroci;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.content.Intent;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,11 +20,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     public void prijatnoOnClick(View view) {
         test++;
         TextView t = (TextView) findViewById(R.id.tvDorucak);
         t.setText("Dorucak: " + test);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.action_edit:
+                Intent intent = new Intent(this, Edit.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
